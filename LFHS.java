ecf722898000a05b1d0ec12c90d0d1757595be37
RJRolander <69980870+RJRolander@users.noreply.github.com>
2021-01-25 13:41:53 -0600

    Update LFHS.java
    Fixed

--- LFHS.java
+++ LFHS.java
@@ -1,1 +1,29 @@
+public class LFHS {
+  private int numCars;
+  public final int MAX_CARS = 400
+
+  public ParkingLot() {
+    numCars = 0;
+  }
+
+  public ParkingLot(int initnumCars) {
+    numCars = initnumCars;
+  }
+
+  public int getNumCars() {
+    return numCars;
+  }
+
+  public void setNumCars (int theNumCars) {
+    if (theNumCars >= 0 && theNumCars <= MAX_CARS)
+    numCars = theNumCars;
+  }
+
+  public ParkingLot fillLot() {
+    ParkingLot p = (ParkingLot)other;
+    while (p <= 400) {
+      p++;
+    }
+  }
+}
 
